d53a379f153faa536796d47868e580ec963b11d6
luani86 <36072798+luani86@users.noreply.github.com>
2018-02-07 00:30:57 +0100

    Add files via upload

---
+++ loop_homework/loops1.js
@@ -0,0 +1,8 @@
+var i;
+var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var square = (a[i] * a[i]);
+var sum = 0;
+for(i = 0; i < 20; i++){
+sum = a[i] * a[i];
+}
+console.log(sum);
