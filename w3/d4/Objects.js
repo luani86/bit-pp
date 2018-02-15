5d60e9d18138d3269f7cc965a7625d6c534c713e
BIT Student <classroom@bgit.rs>
2018-02-15 13:06:00 +0100

    Objects

---
+++ w3/d4/Objects.js
@@ -0,0 +1,25 @@
+// zadatak 1
+var breakfast = {
+    name: "Jelo",
+    ingredients: ["milk", "cereals", "nuts"],
+    energy: 900,
+    printInstructions: function(){
+        var result;
+        if(breakfast.energy > 500){
+            result = true;
+        }
+        else{
+            result = false;
+        }
+        return result;
+    }   
+};
+console.log(breakfast.printInstructions());
+
+// zadatak 2
+
+var brekfast= {
+    name: "smth simple",
+    ingredients: 
+
+}
