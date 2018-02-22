ec60dff1ccb83e27f266b1b74a9fec9116e0bf64
luani86 <36072798+luani86@users.noreply.github.com>
2018-02-23 00:12:11 +0100

    Add files via upload
    Festival doradjen

--- w4/d4/festival.js
+++ w4/d4/festival.js
@@ -5,29 +5,36 @@ console.log("Hi");
 return
 function Genre(){
 var name = this.name;
-this.getData = function(){
+this.getData = (function(){
     var acronym = name[0].toUpperCase() + name[name.length - 1].toUpperCase();
-}
+    return acronym;
+})()
 }
 
 function Movie(){
     var title = this.title;
     var genre = Genre();
     var length = this.length;
+    this.getData = (function(){
+        var movieData = title + length + Genre.getData;
+        return movieData;
+    })()
     }
 
     function Program(){
         var date = this.date;
-        var list = this.list[];
+        var list = this.list;
         var length = this.length;
-        var totalNumOfMovies = this.totalNumOfMovies[];
+        var totalNumOfMovies = this.totalNumOfMovies;
         }
 
         function Festival(){
             var name = this.name;
-            var listOfPrograms = this.listOfPrograms[];
+            var listOfPrograms = this.listOfPrograms;
             var length = this.length;
-            var numOfMovies = this.numOfMovies[];
+            var numOfMovies = this.numOfMovies;
             }
+
+            
 
 }())
