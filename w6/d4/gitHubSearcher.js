9e2a3784f97c4f5c09fd5d6c548c3727eb2e6edb
BIT Student <classroom@bgit.rs>
2018-03-08 18:11:13 +0100

    doradjen github searcher

--- w6/d4/gitHubSearcher.js
+++ w6/d4/gitHubSearcher.js
@@ -1,24 +1,29 @@
 $("#searchbox").on("keydown", function (event) {
-
     if (event.keyCode == 13) {
+        event.preventDefault();
 
         var typedText = $("#searchbox").val();
-        console.log(typedText);
+
         var request = $.ajax({
-             url: "https://api.github.com/search/users?q=" + typedText,
-             method: "GET"
+            url: "https://api.github.com/search/users?q=" + typedText,
+            method: "GET"
+        });
+
+        request.done(function (response) {
+            const users = response.items;
+
+            console.log(users);
+            for (let i = 0; i < users.length; i++) {
+                const img = $(`<img src="${users[i].avatar_url}" />`)
+                $("#users").append(img);
+                const login = $(`<p>${users[i].login}<p/>`)
+                $("#users").append(login);
+            }
 
         });
 
-
-        request.done(function (msg) {
-             //$("#users").html(msg);
-             console.log('aaa');
-        });
-
-        request.fail(function(jqXHR, textStatus){
+        request.fail(function (jqXHR, textStatus) {
             alert("Request failed" + textStatus);
         })
     }
-        request.send()
-    });
+});
