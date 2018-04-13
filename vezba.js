fc4a20403da406820cf6ce5093b17055e6a33bf8
luani86 <36072798+luani86@users.noreply.github.com>
2018-04-13 17:38:59 +0200

    Add files via upload

--- vezba.js
+++ vezba.js
@@ -459,7 +459,7 @@ console.log(alternate(["a", "b", "c", "d"], [1, 2, 3, 4]))
 arrayOfDigits = (num) => {
     var stringDigit = num.toString().split("");
     var newArray = [];
-    for(var i=0; i<stringDigit.length; i++) {
+    for (var i = 0; i < stringDigit.length; i++) {
         var parsedString = parseInt(stringDigit[i]);
         newArray.push(parsedString);
     }
@@ -470,15 +470,15 @@ console.log(arrayOfDigits(3457));
 //5 multiplication table up to 12
 multiplicationTable = () => {
     var result = "x ";
-    for(var i=0; i<11; i++) {
-        for(var j=0; j<11; j++ ) {
-            if(i == 0 && j > 0) {
+    for (var i = 0; i < 11; i++) {
+        for (var j = 0; j < 11; j++) {
+            if (i == 0 && j > 0) {
                 result += "[" + i + "]"
             }
-            else if(j == 0 && i > 0) {
+            else if (j == 0 && i > 0) {
                 result += "[" + j + "]"
             }
-            else if(i > 0 && j > 0) {
+            else if (i > 0 && j > 0) {
                 result += (i * j) + " "
             }
         }
@@ -490,18 +490,18 @@ console.log(multiplicationTable())
 
 //6 Centigrade to Fahrenheit
 centToFahr = (temp) => {
-    return (temp * 1.8 + 32) 
+    return (temp * 1.8 + 32)
 }
 console.log(centToFahr(8))
 
 //7 maximum number and filter non numbers
 findMax = (ar) => {
     var max = ar[0]
-    for(var i=0; i<ar.length; i++) {
-        if(typeof ar[i] !== "number") {
+    for (var i = 0; i < ar.length; i++) {
+        if (typeof ar[i] !== "number") {
             return "Wrong array"
         }
-        else if(ar[i] > max) {
+        else if (ar[i] > max) {
             max = ar[i];
         }
     }
@@ -514,12 +514,12 @@ maxAndMin = (ar) => {
     var newAr = [];
     var max = ar[0];
     var min = ar[0];
-    for(var i=0; i<ar.length; i++) {
-       
-        if(ar[i] > max) {
+    for (var i = 0; i < ar.length; i++) {
+
+        if (ar[i] > max) {
             max = ar[i];
         }
-        if(ar[i] < min) {
+        if (ar[i] < min) {
             min = ar[i];
         }
     }
@@ -529,14 +529,14 @@ console.log(maxAndMin([4, 6, 11, 29, 2]));
 
 //9 find median
 findMedian = (ar) => {
-    var sortedAr = ar.sort((a, b) => {return a - b});
+    var sortedAr = ar.sort((a, b) => { return a - b });
     var newAr = [];
     var min = ar[0];
     var median;
-    for(var i=0; i<ar.length; i++) {
-        if(sortedAr.length % 2 !== 0) {
-            median = sortedAr[(sortedAr.length+1) / 2]
-        } else{median = (sortedAr[(sortedAr.length - 2) / 2 ] + sortedAr[(sortedAr.length) / 2]) / 2}
+    for (var i = 0; i < ar.length; i++) {
+        if (sortedAr.length % 2 !== 0) {
+            median = sortedAr[(sortedAr.length + 1) / 2]
+        } else { median = (sortedAr[(sortedAr.length - 2) / 2] + sortedAr[(sortedAr.length) / 2]) / 2 }
     }
     return median;
 }
@@ -545,20 +545,20 @@ console.log(findMedian([4, 2, 5, 1, 3]));
 //10 find mostFrequent??
 //10 find ocuurance?
 mostFrequent = (array) => {
-    if(array.length === 0) {
+    if (array.length === 0) {
         return "prazan niz"
     }
     var newArray = [];
     var maxElement = array[0];
     var maxCount = 1;
-    for(var i=0; i<array.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         var element = array[i];
-        if(newArray[element] == null) {
+        if (newArray[element] == null) {
             newArray[element] = 1;
         } else {
             newArray[element]++
         }
-        if(newArray[element] > maxCount) {
+        if (newArray[element] > maxCount) {
             maxElement = element
             maxCount = newArray[element];
         }
@@ -569,11 +569,11 @@ console.log(mostFrequent(['pear', 'apple', 'orange', 'apple']))
 
 //11 return first middle and last element
 firstMiddleLast = (ar) => {
-    var newArFirst = [ar[0], ar[(ar.length + 1) / 2], ar[ar.length -1]];
-    var newArSecond = [ar[0], ar[ar.length -1]];
-    if(ar.length % 2 !== 0) {
+    var newArFirst = [ar[0], ar[(ar.length + 1) / 2], ar[ar.length - 1]];
+    var newArSecond = [ar[0], ar[ar.length - 1]];
+    if (ar.length % 2 !== 0) {
         return newArFirst;
-    } else {return newArSecond}
+    } else { return newArSecond }
 }
 console.log(firstMiddleLast([4, 6, 11, 29, 2]))
 
@@ -581,12 +581,12 @@ console.log(firstMiddleLast([4, 6, 11, 29, 2]))
 findAverage = (ar) => {
     var sum = 0;
     var count = 0
-    for(var i=0; i<ar.length; i++) {
+    for (var i = 0; i < ar.length; i++) {
         sum = sum + ar[i];
         count = count + 1;
     }
     return sum / count;
-} 
+}
 console.log(findAverage([10, 20, 30]));
 
 //13 greater than average
@@ -594,76 +594,383 @@ greaterAverage = (ar) => {
     var sum = 0;
     var count = 0
     var newAr = [];
-    for(var i=0; i<ar.length; i++) {
+    for (var i = 0; i < ar.length; i++) {
         sum = sum + ar[i];
         count = count + 1;
         var averageValue = sum / count;
     }
-    for(var i=0; i<ar.length; i++) {
-       if (ar[i] > averageValue) {
-           newAr.push(ar[i])
-       }
+    for (var i = 0; i < ar.length; i++) {
+        if (ar[i] > averageValue) {
+            newAr.push(ar[i])
+        }
     }
     return averageValue;
-} 
+}
 console.log(greaterAverage([10, 20, 30, 40, 50, 150]));
 
 //14 BMI
 calculateBMI = (mass, height) => {
     var bmi = mass / (height * height);
     var result
-    if(bmi <= 15) {
+    if (bmi <= 15) {
         result = "Starvation " + bmi;
     }
-    else if(bmi > 15 && bmi <= 17.5) {
+    else if (bmi > 15 && bmi <= 17.5) {
         result = "Anorexic " + bmi;
     }
-    else if(bmi > 17.5 && bmi < 18.5) {
+    else if (bmi > 17.5 && bmi < 18.5) {
         result = "Underweight " + bmi;
     }
-    else if(bmi >= 18.5 && bmi < 25) {
+    else if (bmi >= 18.5 && bmi < 25) {
         result = "Ideal " + bmi;
     }
-    else if(bmi >= 25 && bmi < 30) {
+    else if (bmi >= 25 && bmi < 30) {
         result = "Overweight " + bmi;
     }
-    else if(bmi >= 30 && bmi < 40) {
+    else if (bmi >= 30 && bmi < 40) {
         result = "Obese " + bmi;
     }
-    else if(bmi >= 40) {
+    else if (bmi >= 40) {
         result = "Morbidly obese " + bmi;
     }
     return result;
 }
 console.log(calculateBMI(75, 2.6));
 
-//15 print string per line in a rectangular form
+//15 print string per line in a rectangular form ne radi ???
 frame = (ar) => {
-    var line = "*********" 
- 
+    var line = "*********"
+
     var newResult = ["*********"];
-    
+
     var finalResult;
-    for(var i=0; i<ar.length; i++) {
-        var  result = "* " + ar[i] + " *" ;
-         newResult.push(result)
-         newResult.push('\n')
+    for (var i = 0; i < ar.length; i++) {
+        var result = "* " + ar[i] + " *";
+        newResult.push(result)
+        newResult.push('\n')
     }
     finalResult = newResult.push(line)
     return newResult.toString();
 }
 console.log(frame(["lknhikjhbvuihbvhbh", "World", "in", "a", "frame"]));
 
-//15 print string per line in a rectangular form drugi nacin ???
+//15 print string per line in a rectangular form drugi nacin ne radi ???
 frame = (ar) => {
-    var line = "*********" 
+    var line = "*********"
     var finalResult;
-    for(var i=0; i<ar.length; i++) {
-        var  result = "* " + ar[i] + " *" ;
+    for (var i = 0; i < ar.length; i++) {
+        var result = "* " + ar[i] + " *";
     }
     return result;
 }
 console.log(frame(["Hello", "World", "in", "a", "frame"]));
 
-[1, 2, 3]
-123
+//10 extra create points in space with 3 coordinates (constructor) ne znam da li je ovo dobro???
+function Point(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+var point_1 = new Point(3, 5, 9)
+console.log(point_1)
+var point_2 = new Point(6, 7, 2)
+console.log(point_1)
+getDistance = (a, b) => {
+    return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z))
+}
+console.log(getDistance(point_1, point_2));
+
+//11 es6 practicals find the product of array elements
+product = (a) => {
+    var result = 1;
+    for (var i = 0; i < a.length; i++) {
+        result = result * a[i];
+    }
+    return result;
+}
+console.log(product([2, 8, 3]))
+
+// Built-in-objects
+//1 convert a number from one base to another
+var m = parseInt("ff", 16)
+var y = m.toString(8)
+console.log(y)
+
+//2 reverse a number
+reverseNum = (n) => {
+    n = n + "";
+    return n.split("").reverse().join("");
+}
+console.log(reverseNum(12345))
+
+//3 sort in alphabetical order
+alphabet = (str) => {
+    var lowerCaseStr = str.toLowerCase()
+    var splitStr = lowerCaseStr.split("");
+    return splitStr.sort().join("");
+}
+console.log(alphabet("Webmaster"))
+
+//4 alphabetize words in a string
+alphabetize = (ar) => {
+    var newArray = ar.split(" ")
+    for (var i = 0; i < newArray.length; i++) {
+        newArray[i] = newArray[i].toLowerCase().split("").sort().join("")
+    }
+    return newArray
+}
+console.log(alphabetize("Republic of Serbia"))
+
+//6 shortened string
+abbreviate = (ar) => {
+    var newAr = ar.split(" ")
+    newAr[newAr.length - 1] = newAr[newAr.length - 1].charAt(0) + ".";
+    return newAr.join(" ");
+}
+console.log(abbreviate("John Snow"))
+
+//7 pad a string ???
+
+//8 capitalize first letter
+capitalize = (str) => {
+    var first = str.charAt(0).toUpperCase();
+    newStr = first + str.slice(1);
+    return newStr;
+}
+console.log(capitalize("js string exercises"))
+
+//8 capitalize first letter drugi nacin
+capitalize = (str) => {
+    var first = str.charAt(0).toUpperCase();
+    var newStr = [];
+    var finalStr = newStr.push(first);
+    for (var i = 1; i < str.length; i++) {
+        newStr.push(str[i]);
+    }
+    return newStr.join("");
+}
+console.log(capitalize("js string exercises"));
+
+//9 hide email address
+hideEmail = (email) => {
+    var splitEmail = email.split("@");
+    var firstPartHidden = splitEmail[0].substring(0, 3) + "...@";
+    var hiddenEmail = [];
+    hiddenEmail.push(firstPartHidden);
+    hiddenEmail.push(splitEmail[1])
+    return hiddenEmail.join("")
+}
+console.log(hideEmail("somerandomaddress@example.com"))
+
+//10 swap uppercase to lowercase and vice versa
+swap = (str) => {
+    var splitStr = str.split("")
+    for (var i = 0; i < splitStr.length; i++) {
+        if (splitStr[i] == splitStr[i].toUpperCase()) {
+            splitStr[i] = splitStr[i].toLowerCase()
+        }
+        else if (splitStr[i] == splitStr[i].toLowerCase()) {
+            splitStr[i] = splitStr[i].toUpperCase()
+        }
+    }
+    return splitStr.join("");
+}
+console.log(swap("The Quick Brown Fox"))
+
+//functions-practical-2
+//1 check if input is string
+ifString = (input) => {
+    if (typeof input == "string") {
+        return true;
+    } else { return false }
+}
+console.log(ifString("tt"))
+
+//2 check if string is blank
+ifBlank = (input) => {
+    if (input == " ") {
+        return true;
+    } else { return false }
+}
+console.log(ifBlank("nn nn mm"))
+
+//3 concatenate string n times
+concatenate = (str, n) => {
+    var array = [];
+    for (var i = 0; i < n; i++) {
+        array[i] = str;
+    }
+    if (n <= 0 || n == undefined) {
+        return "Please enter number.";
+    }
+    return array.join("");
+}
+console.log(concatenate("Ha", 2));
+
+//4.a find number of letters in a string neki bzvz nacin
+countLetters = (str) => {
+    var counter = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == str[i]) {
+            counter++
+        }
+    }
+    return counter;
+}
+console.log(countLetters("ggg"))
+
+//4 find letter occurance
+findOccurance = (str, element) => {
+    var counter = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == element || str[i] == element.toUpperCase() || str[i].toUpperCase() == element) {
+            counter = counter + 1
+        }
+    }
+    return counter;
+}
+console.log(findOccurance("My random string", "m"))
+
+//5 find first occurance of a character
+findFirstOccurance = (str, element) => {
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == element || str[i] == element.toUpperCase() || str[i].toUpperCase() == element) {
+            return (i + 1);
+        }
+    }
+    if (str[i] !== element) {
+        return -1;
+    }
+
+}
+console.log(findFirstOccurance("My random string", "y"));
+
+//6 find last occurance of a character
+findLastOccurance = (str, element) => {
+    var result;
+    var result = str.lastIndexOf(element) + 1
+    if (result == undefined || result == 0) {
+        result = -1;
+    }
+    return result;
+}
+console.log(findLastOccurance("mmmrr", "r"));
+
+//7 convert string to array
+toArray = (str) => {
+    var splitStr = str.split("")
+    for (var i = 0; i < splitStr.length; i++) {
+        if (splitStr[i] == " ") {
+            splitStr[i] = null;
+        }
+    }
+    return splitStr
+}
+console.log(toArray("My ra sg"));
+
+//8 check if a given number is prime number ???
+isPrime = (n) => {
+    for (var i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    if(n == 1) {
+        return false;
+    }
+    return true;
+}
+console.log(isPrime(379))
+
+//8.b find dividers
+findDividers = (n) => {
+    var dividers = [];
+    for (var i = 2; i < n; i++) {
+        if (n % i == 0) {
+            dividers.push(i)
+        }
+    }
+    return dividers;
+}
+console.log(findDividers(16));
+
+//9 replace spaces with separator
+separator = (str, sep) => {
+    var splitStr = str.split(" ")
+    for(var i=0; i<splitStr.length; i++) {
+        if(splitStr[i] === " ") {
+            splitStr[i] = sep;
+        }
+        else if(sep === undefined) {
+            sep = "_";
+        }
+    }
+    return splitStr.join(sep)
+}
+console.log(separator("My random string", "--------"))
+
+//10 Get first n characters of a string and add ...
+getChar = (str, n) => {
+    var newStr = [];
+    if(n === undefined) {
+        n = 3;
+    }
+    for(var i=0; i<n; i++) {
+        newStr.push(str[i]);
+    }
+    if(n >= str.length) {
+        return str
+    }
+    newStr.push("...");
+    return newStr.join("");
+}
+console.log(getChar("abcdefghijkl", 4))
+
+//11 filter non-numeric values
+filterNum = (ar) => {
+    var newAr = [];
+    for(var i=0; i<ar.length; i++) {
+        if(typeof ar[i] == "number") {
+            newAr.push(ar[i]);
+        }
+    }
+    return newAr;
+}
+console.log(filterNum([1, 21, undefined, "jjkn", 42, 1e+3, Infinity, 56, NaN]));
+
+//12 calculate years until retirement
+retire = (birth, year, sex) => {
+    var result;
+    if(sex == "m") {
+        result = birth + 65 - year;
+    } 
+    else if (sex == "f") {
+        result = birth + 60 - year;
+    }
+    else if(sex !== "m" || sex !== "f") {
+        result = "If you are male enter m, if you are female, enter f!"
+    }
+    if(result <= 0) {
+        result = "You are already retired!"
+    }
+    return result;
+}
+console.log(retire(2040, 2060, "m"))
+
+//functions practical 4
+//10 insert element on a given position
+insertElement = (e, p, ar) => {
+    var newAr = [];
+    if(p >= ar.length) {
+        return "enter correct number"
+    }
+    for(var i=0; i<p; i++) {
+        newAr.push(ar[i])
+    }
+    newAr.push(e);
+    for(i=p; i<ar.length; i++) {
+        newAr.push(ar[i])
+    }
+    return newAr;
+}
+console.log(insertElement(4, 1, [1, 2, 3, 4, 5]))
